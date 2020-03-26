671f40831a4f4821bf5a9168cb83d32fe706743a
Josh Blackburn <whitefreeze@hotmail.com>
2020-03-26 12:48:15 -0500

    fix chrome img animation & optimize img size

--- js/script.js
+++ js/script.js
@@ -1,67 +1,52 @@
+// allows semantic ui to style cards
 $('.special.cards .image').dimmer({
   on: 'hover'
 });
 
 $('.url.example .ui.embed').embed();
 
-$('#certs').visibility({    // grab parent container
-  onBottomVisible: function(calculations) {
-      console.log('Shabooom');
-      $('.fluid.card')    // grab individuals under parent
-      .transition({
-        animation : 'swing down', 
-        interval : 200
-      })
-    }
-  });
-
+// animates elements in in Skills section
 $('#skills').visibility({    // grab parent container
   onOnScreen: function(calculations) {
-      console.log('Shabaaam');
-      $('#skills tr td strong')    // grab individuals under parent
-      .transition({
-        animation : 'fade',
-        interval : 100
-      })
-      $('#skills tr td i')    // grab individuals under parent
-      .transition({
-        animation : 'fade',
-        interval : 35
-      })
-    }
-  });
-
-  $('#learning').visibility({    // grab parent container
-  onOnScreen: function(calculations) {
-      console.log('Shapooo');
-      $('.status')    // grab individuals under parent
-      .transition({
-        animation : 'fade',
-        interval : 300
-      })
-      $('.year, .course, .tutor')    // grab individuals under parent
-      .transition({
-        animation : 'fade',
-        interval : 100
-      })
-
-
-/* 
-('#upperDiv')
-.visibility({
-onTopPassed: function(calculations) {
-  console.log('Shabooom');
-  $('#achievementsTitleDiv')
-  .velocity("transition.slideUpBigIn", { stagger: 250 })
-  .delay(750)
-  .velocity({ opacity: 1 }, 750)
-  $('#achievementsDiv')
-  .velocity("transition.slideUpBigIn", { stagger: 250 })
-  .delay(750)
-  .velocity({ opacity: 1 }, 750)
-
-}
+    console.log('Shabaaam');
+    $('#skills tr td strong')    // grab individuals under parent
+    .transition({
+      animation : 'fade',
+      interval : 100
+    })
+    $('#skills tr td i')    // grab individuals under parent
+    .transition({
+      animation : 'fade',
+      interval : 35
+    })
+  }
 });
- */
 
+// animates elements in in Learning section
+$('#learning').visibility({    // grab parent container
+onOnScreen: function(calculations) {
+    console.log('Shapooo');
+    $('.status')    // grab individuals under parent
+    .transition({
+      animation : 'fade',
+      interval : 300
+    })
+    $('.year, .course, .tutor')    // grab individuals under parent
+    .transition({
+      animation : 'fade',
+      interval : 100
+    })
+  }
+});
 
+// animates elements in in Learning section
+$('#certs').visibility({    // grab parent container
+  onBottomVisible: function(calculations) {
+    console.log('Shabooom');
+    $('.fluid.card')    // grab individuals under parent
+    .transition({
+      animation : 'fade', 
+      interval : 200
+    })
+  }
+});
